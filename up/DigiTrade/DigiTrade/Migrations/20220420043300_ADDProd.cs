ce5f29afae29281ac8eabe86ef821d193d9fdeb7
prachipatidar <85191809+prachipatidar@users.noreply.github.com>
2022-04-20 10:24:20 +0530

    Add files via upload

---
+++ up/DigiTrade/DigiTrade/Migrations/20220420043300_ADDProd.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DigiTrade.Migrations
+{
+    public partial class ADDProd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductTitle = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    SalePrice = table.Column<long>(type: "bigint", nullable: false),
+                    Cur_Stock = table.Column<long>(type: "bigint", nullable: false),
+                    Tax = table.Column<long>(type: "bigint", nullable: false),
+                    BrandID = table.Column<int>(type: "int", nullable: false),
+                    Processor = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    RAM = table.Column<long>(type: "bigint", nullable: false),
+                    ROM = table.Column<long>(type: "bigint", nullable: false),
+                    PrimaryCamera = table.Column<long>(type: "bigint", nullable: false),
+                    FrontCamera = table.Column<long>(type: "bigint", nullable: false),
+                    Battery = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Products_Brands_BrandID",
+                        column: x => x.BrandID,
+                        principalTable: "Brands",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales_Invoices",
+                columns: table => new
+                {
+                    InvoiceNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InvoiceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CustomerID = table.Column<int>(type: "int", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    Rate = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales_Invoices", x => x.InvoiceNumber);
+                    table.ForeignKey(
+                        name: "FK_Sales_Invoices_Customers_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sales_Invoices_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandID",
+                table: "Products",
+                column: "BrandID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_Invoices_CustomerID",
+                table: "Sales_Invoices",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_Invoices_ProductID",
+                table: "Sales_Invoices",
+                column: "ProductID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sales_Invoices");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+        }
+    }
+}
---
+++ up/DigiTrade/DigiTrade/Models/Sales Invoice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DigiTrade.Models
+{
+    public class Sales_Invoice
+    {
+        [Key]
+        public int InvoiceNumber { get; set; }    // Primary Key - Auto-incremented
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? InvoiceDate { get; set; }
+
+        //public date InvoiceDate { get; set; }
+       
+        [Display(Name = "Customer")]
+        public virtual int CustomerID { get; set; }
+        [ForeignKey("CustomerID")]
+        public virtual Customer Customer { get; set; }
+
+
+        
+        [Display(Name = "Product")]
+        public virtual int ProductID { get; set; }
+        [ForeignKey("ProductID")]
+        public virtual Product Product { get; set; }
+
+        
+
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "Maximum of 10 Qty  and Minimum 1 Qty only !")]
+        public int Qty { get; set; }
+        public int Rate { get; set; }
+        
+    }
+}
